3b1500bc3cb898c7d11316bc02fef8f46962d24f
yuyao <asas12365414@gmail.com>
2024-01-25 21:34:42 +0800

    初始化

---
+++ src/main/java/com/yuyao/roomiehub/RoomiehubApplication.java
@@ -0,0 +1,13 @@
+package com.yuyao.roomiehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomiehubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomiehubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuyao/roomiehub/RoomiehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyao.roomiehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomiehubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
